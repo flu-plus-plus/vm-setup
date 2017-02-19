8112386437418d30e4baf32bb6fb926e0b859f9e
Sibert Aerts <sibert.aerts@gmail.com>
2017-02-20 00:41:51 +0100

    Add my forks (#1)

--- setup.sh
+++ setup.sh
@@ -7,3 +7,4 @@ bash $current_dir_path/clear-crontab.sh
 bash $current_dir_path/setup-single-user.sh flu-plus-plus bachelorproef flu-plus-plus bachelorproef
 bash $current_dir_path/setup-single-user.sh jonathanvdc bachelorproef jonathanvdc bachelorproef
 bash $current_dir_path/setup-single-user.sh lynn bachelorproef lynnvh bachelorproef
+bash $current_dir_path/setup-single-user.sh Sibert-Aerts bachelorproef Sibert-Aerts bachelorproef
