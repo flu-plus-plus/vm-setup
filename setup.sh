20f04eb2f744338adf8ad6ed48893fb9a4179705
cekefun <cekefun@gmail.com>
2017-02-20 10:06:10 +0100

    Cedric in setup.sh

--- setup.sh
+++ setup.sh
@@ -8,3 +8,4 @@ bash $current_dir_path/setup-single-user.sh flu-plus-plus bachelorproef flu-plus
 bash $current_dir_path/setup-single-user.sh jonathanvdc bachelorproef jonathanvdc bachelorproef
 bash $current_dir_path/setup-single-user.sh lynn bachelorproef lynnvh bachelorproef
 bash $current_dir_path/setup-single-user.sh Sibert-Aerts bachelorproef Sibert-Aerts bachelorproef
+bash $current_dir_path/setup-single-user.sh cekefun bachelorproef Cekefun bachelorproef
